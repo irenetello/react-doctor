b8001c0e73b558b85c4a88f7f7bf3bc20bbed785
Adam Abubakari <Adam Abubakari>
2026-02-21 16:20:11 +0000

    - Fix remaining merge conflict errors

--- src/engine/rules/jsxTagUtils.ts
+++ src/engine/rules/jsxTagUtils.ts
@@ -56,6 +56,7 @@ export function getAttribute(node: ts.JsxOpeningLikeElement, attr: string): stri
     }
   }
   return undefined;
+}
   
 export function getOpeningTagName(content: string, idx: number): string | null {
   const lastLt = content.lastIndexOf("<", idx);
